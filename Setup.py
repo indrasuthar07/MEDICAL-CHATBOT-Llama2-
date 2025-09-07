1de53ce7da7ab53c6522deea8856d47eca5c244b
indar suthar <indrasuthar14@gmail.com>
2025-09-07 22:42:35 +0530

    commit

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Indra Suthar',
+    author_email= 'indrasuthar14@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
